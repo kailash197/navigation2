96a4a60a7429076d44a57bd1df7d58b69e82a8d0
Kailash Khadka <k.khadka343@gmail.com>
2025-01-06 01:45:56 +0000

    CustomPlannerPlugin: all

--- neobotix_mp_400_navigation/path_planner_server/setup.py
+++ neobotix_mp_400_navigation/path_planner_server/setup.py
@@ -18,6 +18,7 @@ setup(
         (os.path.join('share', package_name, 'rviz_config'), glob('rviz_config/*.rviz')),
         (os.path.join('share', package_name, 'config/custom_costmap'), glob('config/custom_costmap/*.yaml')),
         (os.path.join('share', package_name, 'config/my_custom_crazy_costmap'), glob('config/my_custom_crazy_costmap/*.yaml')),
+        (os.path.join('share', package_name, 'config/straightline_planner'), glob('config/straightline_planner/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
